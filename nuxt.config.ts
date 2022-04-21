e416782ec8a784349c1e013ffc68663c33a9e9df
Sebastian Thomas <mail@sthomas.ch>
2022-04-21 10:20:11 +0200

    CSS in public/ instead of assets/

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@ import { defineNuxtConfig } from 'nuxt'
 
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
-  css: ['@/assets/css/main.css'],
+  css: ['@/public/css/main.css'],
   ssr: true,
   target: 'static',
   router: {
